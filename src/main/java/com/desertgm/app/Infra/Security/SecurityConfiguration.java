55d6bdf506560e7249bd7ca381ac513bc0569ec3
desert <gabrielshot1@gmail.com>
2024-06-18 19:31:08 -0300

    alterado para funcionar como http

--- src/main/java/com/desertgm/app/Infra/Security/SecurityConfiguration.java
+++ src/main/java/com/desertgm/app/Infra/Security/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST,"/auth/reset-password").permitAll()
                         .anyRequest().authenticated()
                 )
-                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                //.requiresChannel(channel -> channel.anyRequest().requiresSecure())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
