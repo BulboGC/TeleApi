8e4774e02347d4363f0d8a0134bdc00293a3a48e
Gabriel Coutinho <gabrielshot1@gmail.com>
2024-06-18 10:05:52 -0300

    versão 1.0.3 adicionado redirecionamento de http para https

--- src/main/java/com/desertgm/app/Infra/Security/SecurityConfiguration.java
+++ src/main/java/com/desertgm/app/Infra/Security/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST,"/auth/reset-password").permitAll()
                         .anyRequest().authenticated()
                 )
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
